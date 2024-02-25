8ccc5dcd55bf69e7cf1c7cc36c56066a75a851aa
Victorrav <161227008+Victorrav@users.noreply.github.com>
2024-02-25 17:24:10 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,55 @@
+body{
+    background-color: rgb(255, 255, 255);
+    background-image: url(../img/walpaper\ legal\ pra\ pc.jpg);
+    background-repeat: no-repeat;
+   height: 500vh;
+}
+header{  
+    background-color: brown;  
+    border-bottom: 10px solid #000 ; 
+   padding: 10px 20px;
+}
+
+nav{
+    text-align: right;
+}
+a {
+    display: inline-flex;
+    align-items: center;
+    color: aliceblue;
+    font-size: 20px;
+    font-family:"Fira Code",Arial, Helvetica, sans-serif;
+    font-weight: 500;
+    text-decoration: none;
+    margin-right: 30px;
+}
+a:before{
+   content: "";
+   background-color: blueviolet;
+   width: 10px;
+   height: 10px;
+   margin-right: 10px;
+
+}
+span {
+    
+    background-color: blueviolet;
+    width: 10px;
+    height: 10px;
+    margin-right: 10px;
+}
+#button{
+    display: inline-block;
+    background-color: rgb(206, 107, 107);
+    box-shadow: 5px 10px 0 black;
+    padding: 18px 20px;
+    margin-right: 0;
+}
+
+.logo{
+    height: 90px; 
+}
+.sonic{
+    width: 80px;
+    height: 90px;
+}
